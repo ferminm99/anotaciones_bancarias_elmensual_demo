fb2654d013c2282a44bc1b1e7779d45915d9f201
Ferminm99 <ferminmorenoz99@gmail.com>
2025-05-01 22:43:56 -0300

    session ts changes

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,8 +1,10 @@
-let sessionId = localStorage.getItem("session_id");
+export const getSessionId = (): string => {
+  let sessionId = localStorage.getItem("session_id");
 
-if (!sessionId) {
-  sessionId = crypto.randomUUID(); // o usa uuid si querés
-  localStorage.setItem("session_id", sessionId);
-}
+  if (!sessionId) {
+    sessionId = crypto.randomUUID(); // O usa uuid si querés
+    localStorage.setItem("session_id", sessionId);
+  }
 
-export const getSessionId = () => sessionId!;
+  return sessionId;
+};
